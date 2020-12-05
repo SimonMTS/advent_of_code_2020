b64de5d3cd9d8fffb292ade09b4e84144c104fbd
SimonMTS <info@simonstriekwold.nl>
2020-12-05 23:21:25 +0100

    optimized day5

--- day5/day5.go
+++ day5/day5.go
@@ -2,65 +2,44 @@ package main
 
 import (
 	"fmt"
-    "math"
-    "io/ioutil"
+	"io/ioutil"
 	"strings"
+	"strconv"
 )
 
-const split_pos = 18 // 13 / 7 / 18
-const input_file = "./bigboy.txt" // ihs2 / day5_input / bigboy
+const split_pos  = 18 				// 13 	/ 7 		 / 18
+const input_file = "./bigboy.txt" 	// ihs2 / day5_input / bigboy
 
 func main() {
 	bytes , _ := ioutil.ReadFile(input_file)
 	input_strings := strings.Split(string(bytes),"\n")
-	len_input_strings := len(input_strings)
 
 	lowest := int(^uint(0) >> 1)
 	highest := 0
 	sum := 0
-	fmt.Printf("%9d\n", len_input_strings)
+	fmt.Printf("%9d\n", len(input_strings))
 	for i, s := range input_strings {
-		_, _, id := codeToValues(s)
+		id := codeToValues(s)
 		sum += id
 		if id < lowest { lowest = id }
 		if id > highest { highest = id }
 
-		if i % 100000 == 0 {
-			fmt.Printf("%9d", i)
-			fmt.Print("\r")
-		}
+		if i % 100000 == 0 { fmt.Printf("%9d\r", i) }
 	}
 
 	expected_sum := 0
-	for i := lowest; i <= highest; i++ {
-		expected_sum += i
-	}
+	for i := lowest; i <= highest; i++ { expected_sum += i }
 
-	fmt.Print("\nhighest id: "); fmt.Println(highest)
-	fmt.Print("my id: "); fmt.Println(expected_sum - sum)
+	fmt.Print("\n highest id: "); fmt.Println(highest)
+	fmt.Print(" missing id: "); fmt.Println(expected_sum - sum)
 }
 
-func codeToValues(code string) (col, row, id int) {
+func codeToValues(code string) int {
 	res := ""
 	for _, s := range code {
-		if s == 'F' || s == 'L' { res += "1" }
-		if s == 'B' || s == 'R' { res += "0" }
+		if s == 'F' || s == 'L' { res += "0" } else { res += "1" }
 	}
 
-	row = binaryReduce(0, math.Pow(2,split_pos)-1, res[:split_pos])
-	col = binaryReduce(0, math.Pow(2,float64(len(code)-split_pos))-1, res[split_pos:])
-	id = (row * int(math.Pow(2,float64(len(code)-split_pos)))) + col
-	return
-}
-
-func binaryReduce(lower, upper float64, path string) int {
-	if len(path) == 0 { return int(upper) }
-
-	if (path[0] == '1') {
-		upper = upper - math.Ceil((upper - lower) / 2)
-	} else {
-		lower = lower + math.Ceil((upper - lower) / 2)
-	}
-	
-	return int(binaryReduce(lower, upper, path[1:]))
+	id, _ := strconv.ParseInt(res, 2, 64)
+	return int(id)
 }
